79b7b19d0f8d8ac7be7cc776b9c3cfa8456d177e
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-22 15:20:10 -0300

    Inicio sessão 2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,11 @@
 import Header from "../components/Header/Header";
+import TargetAudience from "../components/TargetAudience/TargetAudience";
+import styles from './Home.module.css';
 const Home = () => {
   return (
     <div> 
       <Header />
+     <TargetAudience />
     </div>
   )
 }
---
+++ src/components/TargetAudience/TargetAudience.js
@@ -0,0 +1,11 @@
+import styles from "./TargetAudience.module.css"
+
+const TargetAudience = () => {
+  return (
+    <section className={styles.targetAudience}>
+
+    </section>
+  );
+}
+
+export default TargetAudience;
