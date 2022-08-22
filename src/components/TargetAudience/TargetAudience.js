cdf2a07328390fa6b002c828f722cbbf85f77001
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-22 20:41:38 -0300

    finalizando TargetAudience

--- src/components/TargetAudience/TargetAudience.js
+++ src/components/TargetAudience/TargetAudience.js
@@ -3,7 +3,19 @@ import styles from "./TargetAudience.module.css"
 const TargetAudience = () => {
   return (
     <section className={styles.targetAudience}>
-
+      <div className={styles.containerPublicoAlvo}>
+        <img src="ícone - PÚBLICO-ALVO.png" alt="icone publico alvo" />
+      <p>PÚBLICO ALVO</p>
+      </div>
+      <h2>Para quem é o curso?</h2>
+      <section className={styles.containerImagens}>
+      <img src="./boxIdSet.png" alt="box IDset" />
+      <div className={styles.divImagensTextos}>
+      <img src="./Group51.png" alt="Group51" />
+      <img src="./Group52.png" alt="Group52" />
+      <img src="./Group53.png" alt="Group53" />
+      </div>
+      </section>
     </section>
   );
 }
